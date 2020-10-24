8cb8be0e622db8e8dc2ab6398a19a12ddbbe29f6
Peter Cotton <peter.cotton@microprediction.com>
2020-10-23 22:24:37 -0400

    ver

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="embarrassingly",
-    version="0.0.2",
+    version="0.0.3",
     description="Does one thing",
     long_description=README,
     long_description_content_type="text/markdown",
