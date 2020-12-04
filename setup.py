36f69a2c66e1eb0b61a55c2582bc5eea089c364e
Peter Cotton <peter.cotton@microprediction.com>
2020-12-03 19:14:58 -0500

    reqs

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     test_suite='pytest',
     tests_require=['pytest','optuna'],
     include_package_data=True,
-    install_requires=["wheel","pathlib","smt","tdigest","numpy"],
+    install_requires=["wheel","pathlib","numpy","scipy","scikit-learn","pyDOE2","smt","tdigest","numpy","optuna"],
     entry_points={
         "console_scripts": [
             "embarrassingly=embarrassingly.__main__:main",
